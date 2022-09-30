cc977fa39578f9fce201ae5ea83318c4d695a23a
AyeshaFarid <ayeshafarid2020@namal.edu.pk>
2022-09-29 18:40:04 -0700

    calculator

---
+++ calculator/cal03.py
@@ -0,0 +1,19 @@
+#calculator in python(v0.3)
+def add(num1,num2):
+   return num1+num2
+def sub(num1,num2):
+  return num1-num2
+def mul(num1, num2):
+   return num1 * num2
+def div(num1,num2):
+  return num1 // num2
+def sqroot(num1)
+    return num1**2
+
+#main
+res = add(5,3)
+print(res)
+print(sub(5,3))
+print(mul(5,3))
+print(div(10,5))
+print(sqroot(5))
